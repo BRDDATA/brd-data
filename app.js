01bf71904a40181f3905b497adb45674ea6c3574
sivakrishnanb <sivakrishnan@live.com>
2015-10-23 20:13:36 +0530

    included express routing

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ var app = express();
 
 app.use('/', express.static(__dirname + '/'));
 
-/*app.get('/', function (req, res) {
-  res.send('Hello World!');
-});*/
-
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
