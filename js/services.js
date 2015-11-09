5cfaf343a477a007f56a1bbbb812aa707fd7e4b9
simbuec2005 <simbuec2005@gmail.com>
2015-11-09 21:13:16 +0530

    angular js ui routing added

---
+++ js/services.js
@@ -0,0 +1,18 @@
+var bd = angular.module('bd', ['ui.router']);
+bd.config(['$stateProvider','$urlRouterProvider', function ($stateProvider,$urlRouterProvider) {
+	$urlRouterProvider.otherwise('/business-intelligence');
+    
+    $stateProvider
+        .state('business-intelligence', {
+            url: '/business-intelligence',
+            templateUrl: 'business-intelligence.html'
+        })
+        .state('data-warehousing', {
+            url: '/data-warehousing',
+            templateUrl: 'data-warehousing.html'
+        })
+        .state('cloud', {
+            url: '/cloud',
+            templateUrl: 'cloud.html'
+        })
+}])
